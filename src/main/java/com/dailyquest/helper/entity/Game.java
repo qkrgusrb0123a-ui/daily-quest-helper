c7164365d8895b547470b0feedd66fd198ccbc53
Park HyunGyu <qkrgusrb0123a@gmail.com>
2026-03-27 14:20:01 +0900

    game.java 파일 수정

--- src/main/java/com/dailyquest/helper/entity/Game.java
+++ src/main/java/com/dailyquest/helper/entity/Game.java
@@ -22,6 +22,7 @@ public class Game {
     private String lastDailyResetDate = "";
     private String lastWeeklyResetDate = "";
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
